39ebdefaee7e7de4ca7952369e12896cbe1544d8
vaishnavidindorkar <vaishnavi.dindorkar@gmail.com>
2023-07-31 15:12:02 +0530

    For loop Assignment
    For loop Assignment

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,43 @@
+console.log(`===============Step 1 ==========================`);
+console.log(`--------------Print no 5 to 15 incremented by 1------------------`);
+for (let index = 5; index <=15 ; index++) {
+    console.log(index);
+    
+}
+console.log(`===============Step 2 ==========================`);
+console.log(`--------------Print no 50 to 40 decremented by 1------------------`);
+for (let index = 50; index>=40; index--) {
+   console.log(index);
+    
+}
+console.log(`===============Step 3 ==========================`);
+console.log(`--------------Find first 15 odd nos------------------`);
+for (let index = 1; index < 30; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`===============Step 4 ==========================`);
+console.log(`--------------Find first 10 even nos------------------`);
+for (let index = 0; index < 20; index = index+2) {
+    console.log(index);;
+    
+}
+console.log(`===============Step 5 ==========================`);
+console.log(`--------------Print table of 5------------------`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);;
+    
+}
+console.log(`===============Step 6 ==========================`);
+console.log(`--------------Print table of 10------------------`);
+for (let index = 10; index <=100; index=index+10) {
+ console.log(index);
+    
+}
+console.log(`===============Step 7 ==========================`);
+console.log(`--------------Print table of 10 in reverse order------------------`);
+for (let index=100 ; index >=10 ; index=index-10) {
+    console.log(index);  
+    
+}
+
