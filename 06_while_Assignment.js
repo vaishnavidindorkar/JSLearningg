d92b8a651f3ab14b2e2bda42d00d0340b068fd7d
vaishnavidindorkar <vaishnavi.dindorkar@gmail.com>
2023-07-31 15:24:42 +0530

    while loop Assignment
    while loop Assignment

---
+++ 06_while_Assignment.js
@@ -0,0 +1,49 @@
+console.log(`===============Step 1 =================`);
+console.log(`----------------Print no from 5 to 15 by incrementing by 1--------------`);
+var i = 5 ;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`===============Step 2 =================`);
+console.log(`----------------Print no from 50 to 40 by decrementing by 1--------------`);
+var s=50;
+while (s>=40) {
+console.log(s);
+s--;
+}
+console.log(`===============Step 3 =================`);
+console.log(`----------------Find first 15 odd nos--------------`);
+var odd=1;
+while (odd<=30) {
+    console.log(odd);
+    odd=odd+2;
+}
+console.log(`===============Step 4 =================`);
+console.log(`----------------Find first 10 even nos--------------`);
+var even=2;
+while (even<=20) {
+    console.log(even);
+    even=even+2;
+}
+console.log(`===============Step 5 =================`);
+console.log(`----------------Print table of 5--------------`);
+var table5 = 5;
+while (table5<=50) {
+    console.log(table5);
+    table5=table5+5;
+}
+console.log(`===============Step 6 =================`);
+console.log(`----------------Print table of 10--------------`);
+var table10 = 10;
+while (table10 <=100) {
+    console.log(table10);
+    table10=table10+10;
+}
+console.log(`===============Step 7 =================`);
+console.log(`----------------Print table of 10 in reverse order--------------`);
+var reverseTable10 = 100;
+while (reverseTable10 >=10) {
+    console.log(reverseTable10);
+    reverseTable10=reverseTable10-10;
+}
