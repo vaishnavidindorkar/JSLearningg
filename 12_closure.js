e54f9bcf4f152770ee1fc288287676313473aac4
vaishnavidindorkar <vaishnavi.dindorkar@gmail.com>
2023-08-09 15:28:48 +0530

    Closure , Callback , session storage concept
    Closure , Callback , session storage concept

---
+++ 12_closure.js
@@ -0,0 +1,29 @@
+var globalVariable = 100;
+function outer(){
+    let outerFunVariable = 200;
+    let innerFunction = function(){
+        let innerFunVariable = 300;
+        console.log(innerFunVariable);
+        console.log(outerFunVariable);
+        console.log(globalVariable);
+    }
+    return innerFunction;
+}
+outer()();
+// let innerFun = outer();
+// innerFun();
+
+var globalVariable = 100;
+function outer(){
+    let outerFunVariable = 200;
+    let innerFunction = function(){
+        let innerFunVariable = 300;
+        console.log(innerFunVariable);
+        console.log(outerFunVariable);
+        console.log(globalVariable);
+    }
+    return innerFunction;
+}
+outer()();
+// let innerFun = outer();
+// innerFun();
